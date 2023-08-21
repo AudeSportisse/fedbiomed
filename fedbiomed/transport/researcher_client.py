5ea878832640a3d874fc81e68cc71f5d502cc763
Marc Vesin <marc.vesin@inria.fr>
2023-08-21 14:49:37 +0200

    working test: node side (researcher_client) managing cancel (ctrl-c + stop() ) and errors with threads and tasks

--- fedbiomed/transport/researcher_client.py
+++ fedbiomed/transport/researcher_client.py
@@ -6,6 +6,7 @@ import queue
 import threading
 import sys 
 import signal 
+from enum import Enum
 
 from typing import Callable, Dict
 
@@ -79,6 +80,11 @@ def create_channel(
 
     return channel
 
+class TaskReturnCode(Enum):
+    TRC_UNKNOWN = 0
+    TRC_CANCELLED = 1
+    TRC_ERROR_UNAVAIL = 2
+    TRC_ERROR_TIMEOUT = 3
 
 
 async def task_reader_unary(
@@ -86,7 +92,7 @@ async def task_reader_unary(
         node: str,
         callback: Callable = lambda x: x,
          
-):
+) -> TaskReturnCode:
     """Task reader as unary RPC
     
     This methods send unary RPC to gRPC server (researcher) and get tasks 
@@ -103,8 +109,10 @@ async def task_reader_unary(
     #     REQUEST_DONE.set()
 
     #while not SHUTDOWN_EVENT.is_set():
-        
+
+
     try:
+        trc = TaskReturnCode.TRC_UNKNOWN
         while True:
             #request_iterator = stub.GetTaskUnary(
             #    TaskRequest(node=f"{node}")
@@ -137,99 +145,100 @@ async def task_reader_unary(
     except grpc.aio.AioRpcError as exp:
         print(f"task_reader_unary: grpc.aio exception {exp.code()}")
         if exp.code() == grpc.StatusCode.DEADLINE_EXCEEDED:
-            logger.debug("Stream TIMEOUT Error")
-            await asyncio.sleep(2)
+            trc = TaskReturnCode.TRC_ERROR_TIMEOUT
         elif exp.code() == grpc.StatusCode.UNAVAILABLE:
-            logger.debug("Researcher server is not available, will retry connect in 2 seconds")
-            await asyncio.sleep(2)
+            trc = TaskReturnCode.TRC_ERROR_UNAVAIL
         else:
             raise Exception("Request streaming stopped ") from exp
 
     except asyncio.CancelledError as e:
+        trc = TaskReturnCode.TRC_CANCELLED
         print(f'task_reader_unary: exception cancel: {e}')
         request_iterator_future.cancel()
-        raise asyncio.CancelledError
+        #raise asyncio.CancelledError
     except Exception as e:
         print(f"task_reader_unary: exception generic: {e}")
         request_iterator_future.cancel()
     finally:
         print('task_reader_unary: finally')
+        return trc
+
+#async def task_reader(
+#        stub, 
+#        node: str, 
+#        callback: Callable = lambda x: x
+#        ) -> None:
+#        """Low-level task reader implementation 
+#
+#        This methods launches two coroutine asynchronously:
+#            - Task 1 : Send request stream to researcher to get tasks 
+#            - Task 2 : Iterates through response to retrieve tasks  
+#
+#        Task request iterator stops until a task received from the researcher server. This is 
+#        managed using asyncio.Condition. Where the condition is "do not ask for new task until receive one"
+#
+#        Once a task is received reader fires the callback function
+#
+#        Args: 
+#            node: The ID of the node that requests for tasks
+#            callback: Callback function that takes a single task as an arguments
+#
+#        Raises:
+#            GRPCTimeout: Once the timeout is exceeded reader raises time out exception and 
+#                closes the streaming.
+#        """  
+#        event = asyncio.Event()
+#
+#        async def request_tasks(event):
+#            """Send request stream to researcher server"""
+#            async def request_():
+#                # Send starting request without relying on a condition
+#                count  = 1
+#                logger.info("Sending first request after creating a new stream connection")
+#                state.n = time.time()
+#                yield TaskRequest(node=f"{count}---------{node}")
+#                                
+#                while True:
+#                    # Wait before getting answer from previous request
+#                    await event.wait()
+#                    event.clear()
+#
+#                    #logger.info(f"Sending another request ---- within the stream")   
+#                    state.n = time.time()  
+#                    yield TaskRequest(node=f"{count}---------{node}")
+#                    
+#
+#
+#            # Call request iterator withing GetTask stub
+#            state.task_iterator = stub.GetTask(
+#                        request_()
+#                    )
+#
+#        async def receive_tasks(event):
+#            """Receives tasks form researcher server """
+#            async for answer in state.task_iterator:
+#                
+#                reply += answer.bytes_
+#                # print(f"{answer.size}, {answer.iteration}")
+#                if answer.size != answer.iteration:
+#                    continue
+#                else:
+#                    event.set()
+#                    callback(Serializer.loads(reply))
+#                    reply = bytes()
+#                    
+#                
+#        # Shared state between two coroutines
+#        state = type('', (), {})()
+#
+#        await asyncio.gather(
+#            request_tasks(event), 
+#            receive_tasks(event),
+#            )
 
 
-async def task_reader(
-        stub, 
-        node: str, 
-        callback: Callable = lambda x: x
-        ) -> None:
-        """Low-level task reader implementation 
-
-        This methods launches two coroutine asynchronously:
-            - Task 1 : Send request stream to researcher to get tasks 
-            - Task 2 : Iterates through response to retrieve tasks  
-
-        Task request iterator stops until a task received from the researcher server. This is 
-        managed using asyncio.Condition. Where the condition is "do not ask for new task until receive one"
-
-        Once a task is received reader fires the callback function
-
-        Args: 
-            node: The ID of the node that requests for tasks
-            callback: Callback function that takes a single task as an arguments
-
-        Raises:
-            GRPCTimeout: Once the timeout is exceeded reader raises time out exception and 
-                closes the streaming.
-        """  
-        event = asyncio.Event()
-
-        async def request_tasks(event):
-            """Send request stream to researcher server"""
-            async def request_():
-                # Send starting request without relying on a condition
-                count  = 1
-                logger.info("Sending first request after creating a new stream connection")
-                state.n = time.time()
-                yield TaskRequest(node=f"{count}---------{node}")
-                                
-                while True:
-                    # Wait before getting answer from previous request
-                    await event.wait()
-                    event.clear()
-
-                    #logger.info(f"Sending another request ---- within the stream")   
-                    state.n = time.time()  
-                    yield TaskRequest(node=f"{count}---------{node}")
-                    
-
-
-            # Call request iterator withing GetTask stub
-            state.task_iterator = stub.GetTask(
-                        request_()
-                    )
-
-        async def receive_tasks(event):
-            """Receives tasks form researcher server """
-            async for answer in state.task_iterator:
-                
-                reply += answer.bytes_
-                # print(f"{answer.size}, {answer.iteration}")
-                if answer.size != answer.iteration:
-                    continue
-                else:
-                    event.set()
-                    callback(Serializer.loads(reply))
-                    reply = bytes()
-                    
-                
-        # Shared state between two coroutines
-        state = type('', (), {})()
-
-        await asyncio.gather(
-            request_tasks(event), 
-            receive_tasks(event),
-            )
-
-
+def dummy_callback(x):
+    print(f"In callback {x}")
 
 
 class ResearcherClient:
@@ -255,8 +264,6 @@ class ResearcherClient:
         self._stop_event = threading.Event()
         #self._client_thread = threading.Thread()
 
-        self._request_stop = False
-
     async def connection(self):
         """Create long-lived connection with researcher server"""
         
@@ -281,19 +288,16 @@ class ResearcherClient:
     async def get_tasks(self):
 
         #while not SHUTDOWN_EVENT.is_set():
-        while not self._request_stop:
+        while True:
             logger.info("Sending new task request")
             try:
-                # await task_reader(stub= self._stub, node=NODE_ID, callback=self.on_task)
-                #await task_reader_unary(stub= self._stub, node=NODE_ID, callback= lambda x: x)
-                task = asyncio.create_task(task_reader_unary(stub= self._stub, node=NODE_ID, callback= lambda x: x))
+                #task = asyncio.create_task(task_reader_unary(stub= self._stub, node=NODE_ID, callback= lambda x: x))
+                task = asyncio.create_task(task_reader_unary(stub= self._stub, node=NODE_ID, callback= dummy_callback))
                 #print(f"get_tasks: create {asyncio.current_task()} \n{asyncio.all_tasks()}")
-                #while True:
-                #    await asyncio.sleep(1)
                 while not task.done():
                     print("get_task: waiting for task to complete")
                     await asyncio.wait({task}, timeout=2)
-                print("get_tasl: task completed")
+                print("get_task: task completed")
 
             #except GRPCStop:
             #    # Break gRPC while loop
@@ -311,12 +315,10 @@ class ResearcherClient:
             #    else:
             #        raise Exception("Request streaming stopped ") from exp
 
-            # INFO: seems to be never reached
+            # INFO: seems to be never reached - `wait` always complete even if task is interrupted
             #
             # except Exception:
             #    print("get_tasks: exception")
-            except Exception:
-                print("get_tasks: exception")
             finally:
                 print("get_tasks: finally")
                 #print(f"get_tasks: finally {asyncio.current_task()} \n{asyncio.all_tasks()}")
@@ -325,9 +327,23 @@ class ResearcherClient:
                 while not task.done():
                     await asyncio.sleep(0.1)
                     print(f"get_tasks: finally cancelling: cancelled {task.cancelled()}")
-                #print(f"get_tasks: finally {asyncio.current_task()} \n{asyncio.all_tasks()}")
+                
+                # we can now read the return code (need to catch `asyncio.CancelledError` if not waiting for `task.done()`)
+                res = task.result()
+                match res:
+                    case TaskReturnCode.TRC_UNKNOWN:
+                        print("get_tasks: ERROR bad return code, exiting")
+                        break
+                    case TaskReturnCode.TRC_CANCELLED:
+                        print("get_tasks: cancelled by user, exiting")
+                        break
+                    case TaskReturnCode.TRC_ERROR_UNAVAIL:
+                        logger.debug("Researcher server is not available, will retry connect in 2 seconds")
+                        await asyncio.sleep(2)
+                    case TaskReturnCode.TRC_ERROR_TIMEOUT:
+                        logger.debug("Stream TIMEOUT Error")
+                        await asyncio.sleep(2)
 
-    
     async def send_log(self, log):
 
         _ = await self._log_tub.Feedback(Log(log=log))
@@ -347,7 +363,7 @@ class ResearcherClient:
             #    #asyncio.get_running_loop().close()
             #    print("Cancelled by KeyboardInterrupt")
             except ClientStop:
-                print("Run: caught cancel exception")
+                print("Run: caught user stop exception")
             except Exception as e:
                 print(f"Run: caught exception: {e.__class__.__name__}")
             finally:
